0ab735d3c2adb149e446b0dc080fd7a3be9c32d7
Kaan Er <erkaan1@outlook.com>
2020-12-01 01:09:10 +0300

    Fix about pagination algorithm

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,15 +8,13 @@ class WelcomeController < ApplicationController
     @bookmarks = Bookmark.group('book_id').order('count_all Desc').limit(5).count
 
     #pagination => http://localhost:3000/?page=0
-    @page_count = (share_books % BOOKS_PER_PAGE) == 0 ? (share_books % BOOKS_PER_PAGE) : (share_books % BOOKS_PER_PAGE) + 1
+    @page_count = (share_books % BOOKS_PER_PAGE) == 0 ? (share_books / BOOKS_PER_PAGE) : (share_books / BOOKS_PER_PAGE).to_i + 1
     if share_books <= BOOKS_PER_PAGE
       @page_count = 1
     end
 
-     @page = params.fetch(:page, 0).to_i
-     if (@page <= 0)
-      @page = 0
-     end
-     @books = Book.where('status': 1).offset(@page * BOOKS_PER_PAGE).limit(BOOKS_PER_PAGE)
+    @page = params.fetch(:page, 0).to_i
+    
+    @books = Book.where('status': 1).offset(@page * BOOKS_PER_PAGE).limit(BOOKS_PER_PAGE)
   end
 end
